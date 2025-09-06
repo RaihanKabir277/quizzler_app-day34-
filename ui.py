f01db6695aa74a89cfe29266dae8ca13797c1938
RaihanKabir277 <raihank0198@gmail.com>
2025-09-06 12:16:44 +0600

    score updated added with right and wrong different color into the canvas

--- ui.py
+++ ui.py
@@ -18,14 +18,33 @@ class QuizInterface:
 
         true_img = PhotoImage(file="images/true.png")
         false_img = PhotoImage(file="images/false.png")
-        self.true_btn = Button(image=true_img, highlightthickness=0)
+        self.true_btn = Button(image=true_img, highlightthickness=0, command=self.check_true)
         self.true_btn.grid(row=2, column=0)
-        self.false_btn = Button(image=false_img, highlightthickness=0)
+        self.false_btn = Button(image=false_img, highlightthickness=0, command=self.check_false)
         self.false_btn.grid(row=2, column=1)
         self.get_next_question()
 
         self.window.mainloop()
     
     def get_next_question(self):
+        self.canvas.config(bg="white")
+        self.score_label.config(text=f"Score: {self.quiz.score}/{self.quiz.question_number}")
         q_text = self.quiz.next_question()
         self.canvas.itemconfig(self.question_text, text=q_text)
+
+    def check_true(self):
+        is_right = self.quiz.check_answer("True")
+        self.give_feedback(is_right)
+    def check_false(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
+
+    
+
