ec6c927dfc0e7db5ec579c948ec2379bca8256b0
RaihanKabir277 <raihank0198@gmail.com>
2025-09-06 12:26:14 +0600

    updated quizz app with grapical UI also added here disabled the button after reached out end of the question

--- ui.py
+++ ui.py
@@ -28,9 +28,14 @@ class QuizInterface:
     
     def get_next_question(self):
         self.canvas.config(bg="white")
-        self.score_label.config(text=f"Score: {self.quiz.score}/{self.quiz.question_number}")
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}/{self.quiz.question_number}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text = "You reached the end of the question")
+            self.true_btn.config(state="disabled")
+            self.false_btn.config(state="disabled")
 
     def check_true(self):
         is_right = self.quiz.check_answer("True")
